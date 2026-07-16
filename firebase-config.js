ad76f607f6a736f06075ab243fb9e982fa23e868
agrivalemocambique-2026 <agrivalemocambique@gmail.com>
2026-07-16 14:00:43 +0200

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+// Substitua pelos dados do SEU projeto Firebase
+// (Firebase Console > Configurações do projeto > Seus apps > SDK setup and configuration)
+
+export const firebaseConfig = {
+  apiKey: "COLE_AQUI",
+  authDomain: "COLE_AQUI.firebaseapp.com",
+  projectId: "COLE_AQUI",
+  storageBucket: "COLE_AQUI.appspot.com",
+  messagingSenderId: "COLE_AQUI",
+  appId: "COLE_AQUI"
+};
