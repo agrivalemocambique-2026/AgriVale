0f5dc8bb212fae3f429104b2d9dbdc9886bf77fb
agrivalemocambique-2026 <agrivalemocambique@gmail.com>
2026-07-19 12:10:56 +0200

    Delete firebase-config.js

--- firebase-config.js
+++
@@ -1,8 +0,0 @@
-export const firebaseConfig = {
-  apiKey: "AIzaSyAujemhWV5vf9uVoZpy_A2O-EspgtEzMZY",
-  authDomain: "agrivale-2026.firebaseapp.com",
-  projectId: "agrivale-2026",
-  storageBucket: "agrivale-2026.firebasestorage.app",
-  messagingSenderId: "1078614661155",
-  appId: "1:1078614661155:web:c1e775832cb61b12713996"
-};
