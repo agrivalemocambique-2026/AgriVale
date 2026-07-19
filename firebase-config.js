48a28ce82e2a71b22dff2f0b84b0fb3664a600de
agrivalemocambique-2026 <agrivalemocambique@gmail.com>
2026-07-19 12:12:04 +0200

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyAujemhWV5vf9uVoZpy_A2O-EspgtEzMZY",
+  authDomain: "agrivale-2026.firebaseapp.com",
+  projectId: "agrivale-2026",
+  storageBucket: "agrivale-2026.firebasestorage.app",
+  messagingSenderId: "1078614661155",
+  appId: "1:1078614661155:web:c1e775832cb61b12713996"
+};
